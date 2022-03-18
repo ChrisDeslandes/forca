2645d4a988b18611e0f5a1eea57cfd55e2692b38
Christiano Deslandes Ribeiro <petracconi@gmail.com>
2022-03-18 14:26:45 -0300

    Atualizações em README.md e forca.py

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Jogo da Forca
 
-Jogo da forca em linha de comando feito com Python 3.9.7.
+Jogo da forca na linha de comando feito com Python 3.9.7.
 
 A palavra de cada jogo é gerada de forma aleatória com base em um banco de palavras armazenadas no arquivo palavras.txt.
+
+O programa não suporta palavras com espaços ou acentuadas.
 
 O usuário tem direito a 6 erros antes de ser enforcado no 7º erro.
 
--- forca.py
+++ forca.py
@@ -5,7 +5,6 @@
 import random
 from colorama import init, Fore
 
-
 init()
 
 
@@ -15,11 +14,8 @@ def getSeparador():
 def getAlfabeto():
     return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-
 def entradaDeDados(letrasDisponiveis):
-
     letra = ""
-
     while True:
             letra = input("\n  Digite uma letra (sem acentuação): ").upper()
             try:
@@ -30,18 +26,13 @@ def entradaDeDados(letrasDisponiveis):
                     continue
                 break
             except: print("\n  Entrada inválida!")
-
     return letra
 
-
-
-def jogo(palavras):
-    
+def jogo(palavras):    
     letrasDisponiveis = getAlfabeto()
     erros = ""
     palavra = random.choice(palavras).upper()
     palavraEscondida = "_ " * len(palavra)
-
     while True:
         print("\n  Palavra a ser descoberta: " + Fore.CYAN + palavraEscondida + Fore.RESET + "\n")
         print("  Erros = " + Fore.RED + str(len(erros)) + Fore.RESET + " ==> " + Fore.RED + " - ".join(erros) + Fore.RESET + "\n")
@@ -62,32 +53,21 @@ def jogo(palavras):
 
         if fimDoJogo(palavra, palavraEscondida, len(erros)): break
 
-
-
 def fimDoJogo(palavra, palavraEscondida, numErros):
-
     if numErros == 7:
         print("\n  Você foi enforcado!!! =(\n  Tente novamente!\n\n  A palavra era: " + Fore.CYAN + " ".join(palavra) + Fore.RESET)
         return True
-
     if not "_" in palavraEscondida:
-        print("\n  Parabéns!!! =D\n  Você descobriu a palavra!\n\n  A palavra era: " + Fore.CYAN + palavraEscondida + Fore.RESET)
+        print("\n  Parabéns!!! =D\n  Você descobriu a palavra!\n\n  A palavra era: " + Fore.CYAN + " ".join(palavra) + Fore.RESET)
         return True
-
     return False
 
-
-
 def sair():
-
-    jogar = input("\n  Deseja jogar novamente? (s para sim) ").lower()
-    
+    jogar = input("\n  Deseja jogar novamente? (s para sim) ").lower()    
     if jogar in ["s", "sim"]:
         print("\n" + getSeparador())
         return False
-
     return True
-
 
 
 filePalavras = open("./palavras.txt", "r")
@@ -95,7 +75,6 @@ palavras = filePalavras.read().split(",")
 filePalavras.close()
 
 while True:
-
     print(getSeparador())
     print("\n  Bem-vindo ao Jogo da Forca! Lembre-se, você tem direito a 6 erros!\n")
     print(getSeparador())
